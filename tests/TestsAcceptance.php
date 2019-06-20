00859d4a4c64d3390889e08a794aeeeb5be89cb2
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2019-06-20 11:18:35 +0200

    Creating project

---
+++ tests/TestsAcceptance.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+trait TestsAcceptance
+{
+    protected $fixture;
+
+    /**
+     * @test
+     * @dataProvider acceptanceProvider
+     */
+    public function shouldPassAcceptanceTests($params, $expected)
+    {
+        $actual = $this->fixture->solution(...$params);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    abstract public function acceptanceProvider(): array;
+}
